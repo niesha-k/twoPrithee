5ca4b3d3ce52d84e34527a5e164615accc849ea1
niesha-k <nieshakarthik@outlook.com>
2026-09-11 15:01:59 -0400

    Updated Prithee to have a function that prompts missing words and prints them

--- src/main/java/Prithee.java
+++ src/main/java/Prithee.java
@@ -1,16 +1,47 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Prithee {
-    private int correctLineCount;
-    private int wrongLineCount;
+    private int linesCorrectlySaidCount;
+    private int linesWronglySaidCount;
 
     private final List<String> lines;
 
+    private Random random;
+
     public Prithee(List<String> lines) {
         this.lines = lines;
-        this.correctLineCount = 0;
-        this.wrongLineCount = 0;
+        this.linesCorrectlySaidCount = 0;
+        this.linesWronglySaidCount = 0;
+        this.random = new Random();
+    }
+
+    public int nextLinePlease(){
+        return random.nextInt(lines.size());
+    }
+
+    public int giveRandomWordFromLine(int indexofLine){
+        String[] wordsFromLine = lines.get(indexofLine).split(" ");
+        return random.nextInt(wordsFromLine.length);
+    }
+
+    public void printAndPromptNextWord(int whatsMyNextLine, int whatsMyNextWord){
+        for(int indexofLine = 0; indexofLine < lines.size(); indexofLine++){
+            String[] splitLine = lines.get(indexofLine).split(" ");
+
+            for(int indexofWord = 0; indexofWord < splitLine.length; indexofWord++){
+
+                if(indexofLine == whatsMyNextLine && indexofWord == whatsMyNextWord){
+                    for(int indexOfLetter = 0; indexOfLetter < splitLine[indexofWord].length(); indexOfLetter++){
+                        System.out.print("_");
+                    }
+                } else {
+                    System.out.print((splitLine[indexofWord]));
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
